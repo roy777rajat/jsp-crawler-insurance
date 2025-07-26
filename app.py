000309388caab0b163236819983230cc8ed2b042
Rajat Roy <roy777rajat@gmail.com>
2025-07-26 13:29:27 +0100

    Chnage Dashboard Name

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import streamlit as st
 import streamlit.components.v1 as components
 import json
-from utils.s3_utils import list_jsp_files_s3 as list_jsp_files_s3
+from utils.s3_utils import list_jsp_files_s3 as list_jsp_files_s3,check_aws_connection
 #import utils.s3_utils as s3_utils
 
 
@@ -41,6 +41,10 @@ if is_local_environment():
             return []
 else:
     def list_jsp_files():
+        if not check_aws_connection():
+            st.error("❌ Unable to connect to AWS S3. Please check your credentials.")
+            return []
+    
         try:
             files = list_jsp_files_s3("jsp-legacy-codes")
             files = [f.split("/")[-1] for f in files if f.endswith(".jsp")]
--- utils/s3_utils.py
+++ utils/s3_utils.py
@@ -9,3 +9,13 @@ def get_jsp_html(bucket_name, key):
     s3 = boto3.client("s3")
     response = s3.get_object(Bucket=bucket_name, Key=key)
     return response["Body"].read().decode("utf-8")
+
+def check_aws_connection():
+    try:
+        s3 = boto3.client("s3")
+        s3.list_buckets()
+        print("✅ AWS connection successful.")
+        return True
+    except Exception as e:
+        print(f"❌ AWS connection failed: {e}")
+        return False
