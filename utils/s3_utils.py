f558ea5e4006d6fab77cac62752ba184cfdc88cf
Rajat Roy <roy777rajat@gmail.com>
2025-07-26 13:36:44 +0100

    Chnage Dashboard Name

--- utils/s3_utils.py
+++ utils/s3_utils.py
@@ -1,12 +1,14 @@
 import boto3
+import streamlit as st
+
 
 def list_jsp_files_s3(bucket_name):
-    s3 = boto3.client("s3")
+    s3 = get_s3_client()
     response = s3.list_objects_v2(Bucket=bucket_name)
     return [obj["Key"] for obj in response.get("Contents", []) if obj["Key"].endswith(".jsp")]
 
 def get_jsp_html(bucket_name, key):
-    s3 = boto3.client("s3")
+    s3 = get_s3_client()
     response = s3.get_object(Bucket=bucket_name, Key=key)
     return response["Body"].read().decode("utf-8")
 
@@ -19,3 +21,11 @@ def check_aws_connection():
     except Exception as e:
         print(f"❌ AWS connection failed: {e}")
         return False
+    
+def get_s3_client():
+    return boto3.client(
+        "s3",
+        aws_access_key_id=st.secrets["aws"]["aws_access_key_id"],
+        aws_secret_access_key=st.secrets["aws"]["aws_secret_access_key"],
+        region_name=st.secrets["aws"]["region"]
+    )
